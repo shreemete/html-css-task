b63d98fedd3d33a1c980d9908dfbdc6dff6fdccd
shreemete <64472247+shreemete@users.noreply.github.com>
2023-07-13 12:46:53 +0100

    validation updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
 function valid() {
     let mail = document.getElementById("email").value;
     let pass = document.getElementById("pass").value;
-    console.log(mail, pass);
+
+    if (mail == "") {
+        document.getElementById("fillEmail").style.display = "flex";
+        document.getElementById("invalidEmail").style.display = "none";
+        if (pass == "") {
+            document.getElementById("fillPass").style.display = "flex";
+            document.getElementById("invalidPass").style.display = "none";
+            return false;
+        } else if (pass != "Pass@123") {
+            document.getElementById("invalidPass").style.display = "flex";
+            document.getElementById("fillPass").style.display = "none";
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    else if (mail != "sankey@gmail.com") {
+        if (!mail.includes("@") || !mail.includes(".com")) {
+            document.getElementById("invalidSyntax").style.display = "flex";
+            document.getElementById("invalidEmail").style.display = "none";
+            document.getElementById("fillEmail").style.display = "none";
+        } else {
+            document.getElementById("invalidEmail").style.display = "flex";
+            document.getElementById("fillEmail").style.display = "none";
+            document.getElementById("invalidSyntax").style.display = "none";
+
+        }
+
+        if (pass == "") {
+            document.getElementById("fillPass").style.display = "flex";
+            document.getElementById("invalidPass").style.display = "none";
+            return false;
+        } else if (pass != "Pass@123") {
+            document.getElementById("invalidPass").style.display = "flex";
+            document.getElementById("fillPass").style.display = "none";
+            return false;
+
+        } else {
+            document.getElementById("invalidEmail").style.display = "none";
+            return false;
+        }
+    }
+    else if (mail == "sankey@gmail.com") {
+
+        document.getElementById("invalidEmail").style.display = "none";
+        document.getElementById("fillEmail").style.display = "none";
+        document.getElementById("invalidSyntax").style.display = "none";
+        if (pass == "") {
+            document.getElementById("fillPass").style.display = "flex";
+            document.getElementById("invalidPass").style.display = "none";
+            return false;
+        } else if (pass != "Pass@123") {
+            document.getElementById("invalidPass").style.display = "flex";
+            document.getElementById("fillPass").style.display = "none";
+            return false;
+
+        }
+    }
+
+
 }
